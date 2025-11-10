7059e7ff605c4d2ff75c2fce0a81fee1bfcadfd6
Sajana4197 <158558553+Sajana4197@users.noreply.github.com>
2025-11-11 00:41:33 +0530

    Added background video

--- frontend-vendor/tailwind.config.js
+++ frontend-vendor/tailwind.config.js
@@ -1,9 +1,20 @@
 import withMT from "@material-tailwind/react/utils/withMT";
 
-export default withMT({
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "fade-in": "fadeIn 1s ease-in-out",
+        bounce: "bounce 2s infinite",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+    },
   },
   plugins: [],
-});
+};
